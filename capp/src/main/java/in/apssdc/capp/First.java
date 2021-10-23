19583a1fb4607626810483f4defdf8d537dbf5d3
vmns <48618479+vmns@users.noreply.github.com>
2021-10-23 11:49:13 +0530

    declared variables

--- capp/src/main/java/in/apssdc/capp/First.java
+++ capp/src/main/java/in/apssdc/capp/First.java
@@ -8,6 +8,10 @@ public class First {
 	}
 	public void m1()
 	{
+		int a = 10;
+		int b = 20;
+		System.out.println("sum of a + b:");
 		System.out.println("This is m1() method in the first class");
+		
 	}
 }
