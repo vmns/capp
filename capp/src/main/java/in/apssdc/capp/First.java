49f72db68871371895f8326a08e0650003ec2dd7
naga sekhar <sekharnaga939@gmail.com>
2021-10-23 11:01:37 +0530

    Added first and second classes

---
+++ capp/src/main/java/in/apssdc/capp/First.java
@@ -0,0 +1,9 @@
+package in.apssdc.capp;
+
+public class First {
+
+	public void Firstmethod()
+	{
+		System.out.println("This is first method in first class");
+	}
+}
---
+++ capp/src/main/java/in/apssdc/capp/second/Second.java
@@ -0,0 +1,9 @@
+package in.apssdc.capp.second;
+
+public class Second {
+
+	public void secondMethod()
+	{
+		System.out.println("This is my second method in second class");
+	}
+}
